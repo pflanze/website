c524ec547e89e65e1aece743084e1335dc3991a3
Christian Jaeger <ch@christianjaeger.ch>
2024-01-15 14:57:45 +0100

    time_util: add ApproximateDisplay

--- src/time_util.rs
+++ src/time_util.rs
@@ -1,4 +1,4 @@
-use std::time::{SystemTime, Instant};
+use std::time::{SystemTime, Instant, Duration};
 
 use chrono::{self, DateTime, NaiveDate, Datelike, TimeZone, NaiveDateTime, Utc};
 
@@ -56,3 +56,73 @@ pub fn now_unixtime() -> i64 {
     now_unixtime as i64
 }
 
+
+// ------------------------------------------------------------------
+
+/// Display in a manner that is nice to read for a human, even if
+/// approximate, not precise.
+pub trait ApproximateDisplay {
+    fn to_approximate_display_string(self) -> String;
+}
+
+impl ApproximateDisplay for Duration {
+    fn to_approximate_display_string(self) -> String {
+        let s = self.as_secs();
+        if s < 60 {
+            format!("{s} second{}",
+                    if s == 1 { "" } else { "s" })
+        } else if s < 3600 {
+            let m = s / 60;
+            format!("{m} minute{}",
+                    if m == 1 { "" } else { "s" })
+        } else if s < 3600*24 {
+            let h = s / 3600;
+            format!("{h} hour{}",
+                    if h == 1 { "" } else { "s" })
+        } else if s < 3600*24*30 { // XX precision?
+            let d = s / (3600*24);
+            format!("{d} day{}",
+                    if d == 1 { "" } else { "s" })
+        } else if s < 3600*24*365 {
+            let m = s / (3600*24*30); // XX precision?
+            format!("{m} month{}",
+                    if m == 1 { "" } else { "s" })
+        } else {
+            let y = s / (3600*24*365);
+            format!("{y} year{}",
+                    if y == 1 { "" } else { "s" })
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn t_rough() {
+        assert_eq!(Duration::from_secs(0).to_approximate_display_string(),
+                   "0 seconds");
+        assert_eq!(Duration::from_secs(59).to_approximate_display_string(),
+                   "59 seconds");
+        assert_eq!(Duration::from_secs(60).to_approximate_display_string(),
+                   "1 minute");
+        assert_eq!(Duration::from_secs(60*30).to_approximate_display_string(),
+                   "30 minutes");
+        assert_eq!(Duration::from_secs(60*60).to_approximate_display_string(),
+                   "1 hour");
+        assert_eq!(Duration::from_secs(60*60*24*3-1).to_approximate_display_string(),
+                   "2 days");
+        assert_eq!(Duration::from_secs(60*60*24*30-1).to_approximate_display_string(),
+                   "29 days");
+        assert_eq!(Duration::from_secs(60*60*24*30).to_approximate_display_string(),
+                   "1 month");
+        // Sun Jan 15 01:00:00 CET 2023 - Mon 15 Jan 01:00:00 CET 2024
+        assert_eq!(
+            Duration::from_secs(1705276800 - 1673740800).to_approximate_display_string(),
+            "1 year");
+        assert_eq!(
+            Duration::from_secs(1705276800 - 1673740800 - 1).to_approximate_display_string(),
+            "12 months");
+    }
+}
