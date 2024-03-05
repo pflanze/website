630347c46f5f9a72e66e155fbc925330af8c7886
Christian Jaeger <ch@christianjaeger.ch>
2024-03-05 22:30:11 +0100

    access_control: add `get_user_from_session_id`

--- src/access_control.rs
+++ src/access_control.rs
@@ -9,13 +9,13 @@ pub mod sqliteposerror;
 
 use crate::{access_control::trimcheck::{trimcheck_username, trimcheck_password},
             hash_util::{verify_password, HashingError},
-            def_boxed_thiserror};
+            def_boxed_thiserror, warn};
 use self::{db::access_control_transaction,
            types::User,
            trimcheck::InputCheckFailure,
            transaction::{TransactionError, TransactError},
            util::UniqueError,
-           sqliteposerror::SQLitePosError};
+           sqliteposerror::SQLitePosError, statements_and_methods::sessionid_hash};
 
 def_boxed_thiserror!(CheckAccessError, pub enum CheckAccessErrorKind {
     #[error("checking access: password hashing error")]
@@ -49,6 +49,40 @@ pub fn check_username_password(
     }) {
         Ok(v) => Ok(v),
         Err(e) => match e {
+            TransactError::TransactionError(e) => Err(e.into()),
+            TransactError::HandlerError(e) => Err(e.into())
+        }
+    }
+}
+
+
+pub fn get_user_from_session_id(
+    session_id: &str, hasher: blake3::Hasher
+) -> Result<Option<User>, CheckAccessError> {
+    let hash = sessionid_hash(hasher, session_id);
+    match access_control_transaction(false, |trans| -> Result<Option<User>, CheckAccessError> {
+        if let Some(sessiondata) = trans.get_sessiondata_by_sessionid_hash(&hash)? {
+            if let Some(id) = sessiondata.user_id {
+                // XX why does get_user_by_id not expect UserId ?
+                if let Some(user) = trans.get_user_by_id(id.0)? {
+                    Ok(Some(user))
+                } else {
+                    warn!("User deleted while session is still active?");
+                    // (Yeah, you definitely need persistent auto-increment!)
+                    Ok(None)
+                }
+            } else {
+                warn!("SessionData without user id, not logged in");//
+                Ok(None)
+            }
+        } else {
+            // warn!("session expired"); // rather just not logged in yet
+            Ok(None)
+        }
+    }) {
+        Ok(v) => Ok(v),
+        Err(e) => match e {
+            // XX what were the reasons/details here ?
             TransactError::TransactionError(e) => Err(e.into()),
             TransactError::HandlerError(e) => Err(e.into())
         }
