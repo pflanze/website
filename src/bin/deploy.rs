6d03ac6b2b9c4db012d5449b075893f27ae4f884
Christian Jaeger <ch@christianjaeger.ch>
2024-03-13 23:26:20 +0100

    deploy: remove use-once abstractions
    Leaving in `gls` as that one is more complex and might be useful to
    reuse. And one abstraction in this file is manageable, OK?

--- src/bin/deploy.rs
+++ src/bin/deploy.rs
@@ -2,24 +2,15 @@
 use anyhow::Result;
 use website::{scripting::{capture_strings, xrun}, stringsplit::StringSplit};
 
-
-fn make(target: &str) -> Result<()> {
-    xrun("make", &[target])
-}
-
-fn cargo(args: &[&str]) -> Result<()> {
-    xrun("cargo", args)
-}
-
 fn gls(dirpath: &str) -> Result<StringSplit> {
     capture_strings("git", &["ls-files", "-z", "--", dirpath], "\0")
 }
 
 
 fn main() -> Result<()> {
-    make("test_website")?;
+    xrun("make", &["test_website"])?;
 
-    cargo(&["build", "--release", "--bin", "website", "--bin", "access_control"])?;
+    xrun("cargo", &["build", "--release", "--bin", "website", "--bin", "access_control"])?;
 
     let mut files = vec!["deploy-receive",
                          "resources/merged/elements/",
