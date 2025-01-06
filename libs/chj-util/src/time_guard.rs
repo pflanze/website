3e80f14dc886fbd2fec65035415fc1d737c664df
Christian Jaeger <ch@christianjaeger.ch>
2025-01-06 13:59:04 +0100

    time_guard: only enable via `TIME_GUARD` env var or `enabled_set(true)`

--- libs/chj-util/src/time_guard.rs
+++ libs/chj-util/src/time_guard.rs
@@ -1,8 +1,39 @@
-//! Tools for performance debugging.
+//! # Tools for performance debugging.
 
-use std::{time::Instant, fmt::Debug};
+//! `time!` is currently always enabled. `time_guard!` is only enabled
+//! if the `TIME_GUARD` env var is set to a truthy value or
+//! `enabled_set(true)` was called in the thread.
+
+//! `time!` and `time_guard!` can also be statically disabled
+//! (compiled out completely) by prefixing their names with `no`.
+
+use std::{time::Instant, fmt::Debug, cell::Cell, os::unix::prelude::OsStrExt};
+
+fn time_guard_env_get() -> bool {
+    match std::env::var_os("TIME_GUARD") {
+        Some(v) => match v.as_bytes() {
+            b"0" | b"" | b"off" | b"false" | b"no" => false,
+            _ => true
+        }
+        None => false
+    }
+}
+
+thread_local!{
+    pub static ENABLED: Cell<bool> = Cell::new(time_guard_env_get());
+}
+
+/// Enable `time_guard!`.
+pub fn enabled_set(on: bool) {
+    ENABLED.with(|cell| cell.set(on))
+}
+
+pub fn enabled() -> bool {
+    ENABLED.with(|old| old.get())
+}
 
 
+// XX also enable/disable?
 #[macro_export]
 macro_rules! time {
     ($name:expr; $($code:tt)*) => {{
@@ -28,24 +59,36 @@ macro_rules! notime {
 
 // Reminiscent of dt.rs (DtGuard)
 
-pub struct TimeGuard<S: Debug> {
-    pub name: S,
-    pub start: Instant
+pub enum TimeGuard<S: Debug> {
+    Disabled,
+    Enabled {
+         name: S,
+        start: Instant
+    },
 }
 
 impl<S: Debug> Drop for TimeGuard<S> {
     fn drop(&mut self) {
-        let elapsed = self.start.elapsed();
-        eprintln!("{:?}: {:#?}", self.name, elapsed);
+        match self {
+            TimeGuard::Disabled => (),
+            TimeGuard::Enabled { name, start } => {
+                let elapsed = start.elapsed();
+                eprintln!("{:?}: {:#?}", name, elapsed);
+            },
+        }
     }
 }
 
 #[macro_export]
 macro_rules! time_guard {
     ($namestr:expr) => {
-        let _guard = time_guard::TimeGuard {
-            name: $namestr,
-            start: std::time::Instant::now()
+        let _guard = if $crate::time_guard::enabled() {
+            $crate::time_guard::TimeGuard::Enabled {
+                name: $namestr,
+                start: std::time::Instant::now()
+            }
+        } else {
+            $crate::time_guard::TimeGuard::Disabled
         };
     }
 }
