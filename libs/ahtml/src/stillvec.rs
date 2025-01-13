88ae429fa11caf1e13b6a6b80e191b7e034ae3da
Christian Jaeger <ch@christianjaeger.ch>
2025-01-13 01:35:25 +0100

    Improve docs

--- libs/ahtml/src/stillvec.rs
+++ libs/ahtml/src/stillvec.rs
@@ -33,23 +33,23 @@ impl<T> StillVec<T> {
     pub fn push_within_capacity(&self, value: T) -> Result<(), T> {
         let p = self.0.get();
         // Safe because pushing within capacity will not cause
-        // reallocations, hence will not invalidate other references,
-        // and StillVec is not Sync.
+        // reallocations, hence will not invalidate references
+        // returned by `get`, and StillVec is not Sync.
         unsafe {&mut *p}.push_within_capacity_(value)
     }
 
     pub fn get(&self, i: usize) -> Option<&T> {
         let p = self.0.get();
-        // Safe because we never give mutable access to `Node`s,
-        // and any stored `Node` remains in the region for the
-        // duration of it's lifetime, and, we pre-allocate all
-        // storage via Vec::with_capacity, so the storage will
-        // never be moved.
+        // Safe because we never give mutable access to slots, and any
+        // stored value remains in the region for the duration of its
+        // lifetime or until `exclusive_clear` is called, and we
+        // pre-allocate all storage via `Vec::with_capacity`, so the
+        // storage will never be moved.
         unsafe {&*p}.get(i)
     }
 
-    // This must take &mut self, to ensure no references from `get`
-    // still exist!
+    // This must take `&mut self`, to ensure that no references from
+    // `get` still exist!
     pub fn exclusive_clear(&mut self) {
         self.0.get_mut().clear()
     }
