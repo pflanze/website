f2f02e6317e5433aabccf3c0ba71a667521f34c5
Christian Jaeger <ch@christianjaeger.ch>
2025-04-12 22:33:44 +0200

    webutils_simple: disable warning
    Todo: what to do in the future?

--- libs/ahtml-from-markdown/src/webutils_simple.rs
+++ libs/ahtml-from-markdown/src/webutils_simple.rs
@@ -5,7 +5,7 @@ pub fn email_url(s: &str) -> String {
     if s.starts_with("mailto:") {
         s.into()
     } else if s.starts_with("https:") || s.starts_with("http:") {
-        warn!("using a non-email URL where an email address was expected: {s:?}");
+        // XX warn!("using a non-email URL where an email address was expected: {s:?}");
         s.into()
     } else {
         // hope all is well !
